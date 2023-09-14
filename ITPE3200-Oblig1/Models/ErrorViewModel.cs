3e83905ac295e16ccdcb5c10d8d06fa9483ece27
OliverPreber <s364763@oslomet.no>
2023-09-14 12:10:15 +0200

    Add solution

---
+++ ITPE3200-Oblig1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ITPE3200_Oblig1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
